c4f4dc1f1f22a008be6904dd2bb580aaecf2abd3
mustafaprogramming <mustafaghaffar763@gmail.com>
2024-06-01 18:51:50 +0500

    Adding count down timer

---
+++ build/js/main.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+import chalk from 'chalk';
+console.log(chalk.blue(`\n\n\t   _-^+-^+‾       ◦◦◦◦◦◦     ◦◦◦ ◎ ◉ ◯ ◉ ◎ ◦◦◦     ◦◦◦◦◦◦        ‾+^-+^-_`));
+console.log(chalk.blue(`\t<==!~~ ☆*: .｡. o(≧ ${chalk.greenBright.bold(`Welcome To Mustafa's - Count down timer`)} ≦)o .｡.:*☆ ~~!==>`));
+console.log(chalk.blue(`\t   ‾-∨+-∨+_       ◦◦◦◦◦◦     ◦◦◦ ◎ ◉ ◯ ◉ ◎ ◦◦◦     ◦◦◦◦◦◦        _+∨-+∨-‾\n\n`));
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+console.log(chalk.blue(`\n\n\t   _-^+-^+‾       ◦◦◦◦◦◦     ◦◦◦ ◎ ◉ ◯ ◉ ◎ ◦◦◦     ◦◦◦◦◦◦        ‾+^-+^-_`));
+console.log(chalk.blue(`\t<==!~~ ☆*: .｡. o(≧ ${chalk.greenBright.bold(`Welcome To Mustafa's - Count down timer`)} ≦)o .｡.:*☆ ~~!==>`));
+console.log(chalk.blue(`\t   ‾-∨+-∨+_       ◦◦◦◦◦◦     ◦◦◦ ◎ ◉ ◯ ◉ ◎ ◦◦◦     ◦◦◦◦◦◦        _+∨-+∨-‾\n\n`));
